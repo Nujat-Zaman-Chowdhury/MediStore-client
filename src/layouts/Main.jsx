4a99ea888ba53f68bb47ae081b6bc0439569c6f3
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-06-01 12:29:44 +0600

    added navbar

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../components/Navbar/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="pt-24 min-h-[calc(100vh-68px)]">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
